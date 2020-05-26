1d4566bade9a7dad5ca9c30672fbb96b3e203bf9
David Osollo <davidosollo@localhost.localdomain>
2020-05-26 11:25:51 -0500

    first commit

---
+++ SmartCar/ClientThread.h
@@ -0,0 +1,17 @@
+//
+//  ClientThread.h
+//  SmartCar
+//
+//  Created by David Osollo on 7/13/19.
+//  Copyright © 2019 David Osollo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClientThread : NSThread
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SmartCar/ClientThread.m
@@ -0,0 +1,13 @@
+//
+//  ClientThread.m
+//  SmartCar
+//
+//  Created by David Osollo on 7/13/19.
+//  Copyright © 2019 David Osollo. All rights reserved.
+//
+
+#import "ClientThread.h"
+
+@implementation ClientThread
+
+@end
